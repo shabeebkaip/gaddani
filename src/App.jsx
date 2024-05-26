a0a8d5159d6af6608c515c4c6f094e059c652744
shabeeb k <shabeebkaip@gmail.com>
2024-05-26 21:13:21 +0530

    tag update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-
-import './App.css'
-import coverVideo from './assets/cover_film.mp4'
-import coverVideoVertical from './assets/cover_video_vertical.mp4'
-
+import './App.css';
+import coverVideo from './assets/cover_film.mp4';
+import coverVideoVertical from './assets/cover_video_vertical.mp4';
 
 function App() {
-
-
   return (
-    <div className='bg-black' >
-
-      <video autoPlay loop id="myVideo" src={coverVideoVertical} className='object-cover w-full h-screen block md:hidden ' />
-      <video autoPlay loop id="myVideo" src={coverVideo} className='object-cover w-full h-screen hidden md:block ' />
+    <div className='bg-black'>
+      <>
+        <video autoPlay muted loop id="myVideo" className='object-cover w-full h-screen block md:hidden' >
+          <source src={coverVideoVertical} type="video/mp4" />
+        </video>
+        <video autoPlay muted loop id="myVideo" className='object-cover w-full h-screen hidden md:block' >
+          <source src={coverVideo} type="video/mp4" />
+        </video>
+      </>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
